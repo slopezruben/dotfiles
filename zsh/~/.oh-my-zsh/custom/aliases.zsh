6978696b2a38e810b6a69eb2f4f8e4f702ddcd9a
Ruben <slopez.rubent@gmail.com>
2024-10-08 23:47:29 +0200

    stows + move

--- zsh/~/.oh-my-zsh/custom/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias androidstudio="studio.sh &"
-alias vim=nvim
-alias steamlink="flatpak run com.valvesoftware.SteamLink"
-alias idea="idea.sh &"
-alias forti="$HOME/.bin/forticlientsslvpn/64bit/forticlientsslvpn_cli"
-alias load-esp-idf=". $HOME/Escritorio/Projects/esp-idf/export.sh"
-alias idf="idf.py"
-
-# alias cdtop="cd $(git rev-parse --show-toplevel)"
--- zsh/~/.oh-my-zsh/custom/tmuxifier.zsh
+++
@@ -1,4 +0,0 @@
-# Tmuxifier Conf
-export TMUXIFIER_LAYOUT_PATH="$HOME/.config/tmux/.tmux-layouts"
-eval "$(tmuxifier init -)"
-
